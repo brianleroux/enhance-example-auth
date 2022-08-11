9116626b1d14261240a74dc4e7976e2314ae4bea
brianleroux <b@brian.io>
2022-08-11 14:32:53 -0700

    fix favicon for now

---
+++ app/head.mjs
@@ -0,0 +1,14 @@
+import arc from '@architect/functions'
+
+export default function Head ({ title='' }) {
+  return `
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>${title || ''}</title>
+  <link rel="stylesheet" href="${ arc.static('styles.css') }">
+  <link rel="icon" href="${ arc.static('favicon.svg') }">
+</head>
+`
+}
